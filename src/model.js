14bfba6ba707c18b71a9a59f1937bf1981dde54b
Tyler Walker <jtwalker@ebsco.com>
2018-12-28 17:10:04 -0500

    replaced entries with entities

--- src/model.js
+++ src/model.js
@@ -16,119 +16,120 @@ export const model = (blueprint, json) => (func, ...params) =>
 
 /**
  *
- * Returns all modeled entries
+ * Returns all modeled entities
  *
- * @param {*} entries
+ * @param {*} entities
  */
-export const all = entries => entries;
+export const all = entities => entities;
 
 /**
  *
  * Returns first entry of array
  *
- * @param {*} entries
+ * @param {*} entities
  */
-export const first = entries => entries[0];
+export const first = entities => entities[0];
 
 /**
  *
  * returns last entry of array
  *
- * @param {*} entries
+ * @param {*} entities
  */
-export const last = entries => entries[entries.length - 1];
+export const last = entities => entities[entities.length - 1];
 
 /**
  *
  * curried function that returns filtered array
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} param
  * @param {*} val
  */
-export const findBy = (entries, param, val) => func =>
-  func(entries.filter(entry => entry[param] === val));
+export const findBy = (entities, param, val) => func =>
+  func(entities.filter(entry => entry[param] === val));
 
 /**
  *
  * returns first of filtered "findBy" array
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} param
  * @param {*} val
  */
-export const firstBy = (entries, param, val) =>
-  findBy(entries, param, val)(first);
+export const firstBy = (entities, param, val) =>
+  findBy(entities, param, val)(first);
 
 /**
  *
  * returns last of filtered "findBy" array
  *
- * @param {} entries
+ * @param {} entities
  * @param {*} param
  * @param {*} val
  */
-export const lastBy = (entries, param, val) =>
-  findBy(entries, param, val)(last);
+export const lastBy = (entities, param, val) =>
+  findBy(entities, param, val)(last);
 
 /**
  *
- * curried function that uses callback to filter entries
+ * curried function that uses callback to filter entities
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} callback
  */
-export const findWhere = (entries, callback) => func => func(callback(entries));
+export const findWhere = (entities, callback) => func =>
+  func(callback(entities));
 
 /**
  *
- * returns all entries from "findWhere" function
+ * returns all entities from "findWhere" function
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} callback
  */
-export const findAllWhere = (entries, callback) =>
-  findWhere(entries, callback)(all);
+export const findAllWhere = (entities, callback) =>
+  findWhere(entities, callback)(all);
 
 /**
  *
  * returns first entry from "findWhere" function
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} callback
  */
-export const findFirstWhere = (entries, callback) =>
-  findWhere(entries, callback)(first);
+export const findFirstWhere = (entities, callback) =>
+  findWhere(entities, callback)(first);
 
 /**
  *
  * returns last entry from "findWhere" function
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} callback
  */
-export const findLastWhere = (entries, callback) =>
-  findWhere(entries, callback)(last);
+export const findLastWhere = (entities, callback) =>
+  findWhere(entities, callback)(last);
 
 /**
  *
- * wraps sort method to return sorted entries
+ * wraps sort method to return sorted entities
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} param
  */
-export const sortBy = (entries, param) =>
-  entries.sort((a, b) => (a < b && -1) || (a > b && 1) || 0);
+export const sortBy = (entities, param) =>
+  entities.sort((a, b) => (a < b && -1) || (a > b && 1) || 0);
 
 /**
  *
- * returns new entriesarray after removing entry based on params
+ * returns new entitiesarray after removing entry based on params
  *
- * @param {*} entries
+ * @param {*} entities
  * @param {*} param
  * @param {*} val
  */
-export const removeBy = (entries, param, val) =>
-  entries.filter(entry => entry[param] !== val);
+export const removeBy = (entities, param, val) =>
+  entities.filter(entry => entry[param] !== val);
 
 // strip give a smaller more manageable object
