367c00259b3900b52fce047fd351adb63a805eb5
Tyler Walker <jtwalker@ebsco.com>
2019-01-04 10:50:35 -0500

    Added modelChild to model file

--- src/model.js
+++ src/model.js
@@ -6,6 +6,34 @@ import { normalize } from "./normalize";
  * on the second invocation it accepts one of the grouping functions and params
  * if the grouping function requires them
  *
+ * returns a normalized data structure based on suggestions from the Redux documentation
+ * https://redux.js.org/recipes/structuring-reducers/normalizing-state-shape
+ *
+ * e.g.
+ * {
+ *     id: 42,
+ *     name: "Pikachu",
+ *     stats: {
+ *         find: {
+ *             "100": {
+ *                 id: "100",
+ *                 strength: 40
+ *             },
+ *            "101": {
+ *                 id: "101",
+ *                 cuteness: 95,
+ *            },
+ *            "102": {
+ *                 id: "102",
+ *                 agility: 85,
+ *            },
+ *        }
+ *        ids: ["100", "101", "102"]
+ *     }
+ * }
+ *
+ *
+ *
  * @param {*} blueprint
  * @param {*} json
  */
@@ -13,6 +41,18 @@ export const model = (blueprint, json, aggregates) => (func, ...params) =>
   func
     ? func(normalize(json, blueprint, aggregates), ...params)
     : all(normalize(json, blueprint, aggregates));
+
+/**
+ *
+ * takes a schema blueprint and the path to the child being normalized
+ *
+ * @param {} blueprint
+ * @param {*} json
+ */
+export const modelChild = (blueprint, json, aggregates) => key => {
+  const childModel = model(blueprint, json, aggregates)(all);
+  return { [key]: childModel };
+};
 
 /**
  *
